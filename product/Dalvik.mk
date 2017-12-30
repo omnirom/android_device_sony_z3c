25012dd6373600d6943e21f08244098e7dee8ab0
nailyk-fr <nailyk_git@nailyk.fr>
2017-12-30 12:41:36 +0100

    z3c: Build: Add memory configuration
    Change-Id: I9e8ee9578961835606870e0918988c9f6fd82b70
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

---
+++ product/Dalvik.mk
@@ -0,0 +1,3 @@
+# Dalvik/HWUI
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-hwui-memory.mk)
